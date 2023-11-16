1ae5ba04fa7e4d8068a19647258a409864868168
Mikhail Sergeev <sergeevm100@gmail.com>
2023-11-16 14:15:20 +0300

    feat: adding threads for server and client

--- client.py
+++ client.py
@@ -1,26 +1,91 @@
 import socket
+import logging
+import time
+from argparse import ArgumentParser
+import threading
 
-def simple_client():
-    address = "localhost";
-    port = 8080;
+class ReadThread(threading.Thread):
+    def __init__(self, name, client_socket):
+        super().__init__()
+        self.client_socket = client_socket
+        self.name = name
+        self.bufsize = 8 * 1024
 
+    def run(self):
+        fullbuf = b''
+        while True:
+            buf = self.client_socket.recv(self.bufsize)
+            logging.info('{0} received {1}'.format(self.name, buf))
+            fullbuf += buf
+            if b'1111' in fullbuf:
+                break
+
+def make_connection(name, host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        client_socket.connect((address, port))
-        message = "Hello!"
-        client_socket.sendall(message.encode('utf-8'))
+        client_socket.connect((host, port))
+        recv = client_socket.recv(1)
+        if recv != b'*':
+            logging.error('Error in recv. Did not receive *')
+        logging.info('{0} connected and received: {1}'.format(name, recv))
 
-        data = client_socket.recv(1024)
-        print(f"Recieved {data.decode('utf-8')}")
+        rthread = ReadThread(name, client_socket)
+        rthread.start()
+
+        s = b'^abc$de^abte$f'
+        logging.info('{0} sending {1}'.format(name, s))
+        client_socket.send(s)
+        time.sleep(1.0)
+
+        s = b'xyz^123'
+        logging.info('{0} sending {1}'.format(name, s))
+        client_socket.send(s)
+        time.sleep(1.0)
+
+        s = b'25$^ab0000$abab'
+        logging.info('{0} sending {1}'.format(name, s))
+        client_socket.send(s)
+        time.sleep(0.2)
+
+        rthread.join()
+        client_socket.close()
+        logging.info('{0} disconnecting'.format(name))
 
     except Exception as e:
         print(f"Error: {e}")
 
     finally:
         client_socket.close()
-        print("Connection closed")
+        logging.info('{0} disconnecting'.format(name))
+
+def simple_client():
+    argparser = ArgumentParser('Simple TCP client')
+    argparser.add_argument('-host')
+    argparser.add_argument('-port', type=int)
+    argparser.add_argument('-n', '--num_concurrent', type=int, default=1)
+    args = argparser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG, 
+        format='%(levelname)s:%(asctime)s:%(message)s')
+
+    timer = time.time()
+    connections = []
+
+    for i in range(args.num_concurrent):
+        name = 'conn{0}'.format(i)
+        tconn = threading.Thread(
+            target=make_connection, 
+            args=(name, args.host, args.port))
+
+        tconn.start()
+        connections.append(tconn)
+
+    for conn in connections:
+        conn.join()
+
+    print("Done: ", time.time() - timer)
 
 if __name__ == "__main__":
     simple_client()
-
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,49 +11,118 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-
-#define PORT "8080"
+#define PORT "9090"
 #define BACKLOG 20
+#define BUFFER_SIZE 1024
+
+typedef struct { int sockfd; } thread_config_t;
+
+typedef enum { WAIT_FOR_MSG, IN_MSG } processing_state;
 
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET)
+    if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in *)&sa)->sin_addr);
+    }
 
     return &(((struct sockaddr_in6 *)&sa)->sin6_addr);
 }
 
+void serve_connection(int sockfd)
+{
+    uint8_t buf[BUFFER_SIZE];
+    int len, i;
+
+    if ((send(sockfd, "*", 1, 0)) == -1) {
+        perror("send");
+        exit(3);
+    }
+    
+    processing_state state = WAIT_FOR_MSG;
+
+    while(1) {
+        if ((len = recv(sockfd, buf, sizeof buf, 0)) == -1) {
+            perror("recv");
+            exit(4);
+        }
+        else if (len == 0) {
+            break;
+        }
+        
+        for (i = 0; i < len; i++) {
+            switch(state) {
+                case WAIT_FOR_MSG:
+                    if (buf[i] == '^')
+                        state = IN_MSG;
+                    break;
+                case IN_MSG:
+                    if (buf[i] == '$') {
+                        state = WAIT_FOR_MSG;
+                    }
+
+                    else {
+                        buf[i] += 1;
+                        if ((send(sockfd, &buf[i], 1, 0)) == -1) {
+                            perror("send error");
+                            close(sockfd);
+                            return;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+void* server_thread(void *fd)
+{
+    thread_config_t *config = (thread_config_t *)fd;
+    int sockfd = config->sockfd;
+    free(config);
+
+    unsigned long id = (unsigned long )pthread_self();
+    printf("Thread %lu created to handle connection with socket %d\n", id,
+           sockfd);
+    serve_connection(sockfd);
+    printf("Thread %lu done\n", id);
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    struct sockaddr_storage their_addr;
-    socklen_t addr_size;
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_size;
     struct addrinfo hints, *servinfo, *p;
     int status, sockfd, new_fd;
     char s[INET6_ADDRSTRLEN];
+    thread_config_t *config;
+    int yes = 1;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; // tcp
     hints.ai_flags = AI_PASSIVE; // fill in my IP
 
-    if ((status = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) 
-    {
+    if ((status = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    for (p = servinfo; p != NULL; p->ai_next)
-    {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, 
                              p->ai_socktype, 
-                             p->ai_protocol)) == -1) 
-        {
+                             p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-        {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                        sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
             perror("server: bind");
             continue;
@@ -62,37 +132,46 @@ int main(int argc, char* argv[])
 
     freeaddrinfo(servinfo);
 
-    if (p == NULL)
-    {
+    if (p == NULL) {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1)
-    {
+    if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
         exit(1);
     }
 
     printf("server: waiting for connection...\n");
 
-    while(1)
-    {   
-        addr_size = sizeof their_addr;
+    while(1) {
+        peer_addr_size = sizeof peer_addr;
         if ((new_fd = accept(sockfd, 
-                             (struct sockaddr *)&their_addr, 
-                            &their_addr)) == -1)    
-        {
+                             (struct sockaddr *)&peer_addr, 
+                             &peer_addr_size)) == -1) {
             perror("accept");
             continue;
         }
-        inet_ntop(their_addr.ss_family,
-                  get_in_addr((struct sockaddr *)&their_addr), 
-                  s, sizeof s);    
 
-        if (send(new_fd, "Hello", 5, 0) == -1)
-            perror("send");
-        close(new_fd);
+        inet_ntop(peer_addr.ss_family,
+                  get_in_addr((struct sockaddr *)&peer_addr), 
+                  s, sizeof s);    
+        printf("server: got connection from %s\n", s);
+        
+        pthread_t the_thread;
+
+        if (!(config = (thread_config_t*)malloc(sizeof(*config)))) {
+            perror("config");
+            exit(2);
+        }
+        config->sockfd = new_fd;
+
+        if (pthread_create(&the_thread, NULL, server_thread, config) != 0) {
+            perror("pthread_create");
+            exit(2);
+        }
+
+        pthread_detach(the_thread);    
     }
 
     return 0;
